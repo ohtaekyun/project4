641e684249b58c6166b6dfab1705bc3eb2809b0d
ohtaekyun <ohtaekyun@naver.com>
2023-02-10 09:50:52 +0900

    updated from batch file

--- submit.bat
+++ submit.bat
@@ -1,9 +1,8 @@
-set root=C:\Users\h\Desktop\project4
-
+set git_path=C:\Program Files\Git\bin\git.exe
+set repo_path=C:\Users\h\Desktop\project4
+@REM set root=https://github.com/ohtaekyun/project4
+cd /d %repo_path%
 call git add .
 call git commit -m "updated from batch file"
 call git push
-
-
-
-
+pause
